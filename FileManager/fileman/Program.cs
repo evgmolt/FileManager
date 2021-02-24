6e2f08303f2ee41c2a765d884dc7e285baf37f7b
evgmolt <evg.molt@gmail.com>
2021-02-24 10:32:32 +0400

    Settings

---
+++ FileManager/fileman/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Properties.Settings.Default.DefaultPath == String.Empty)
+            {
+                Properties.Settings.Default.DefaultPath = Directory.GetCurrentDirectory();
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Console.WriteLine(Properties.Settings.Default.DefaultPath);
+            }
+            Console.ReadKey();
+        }
+    }
+}
